47a240096832476d18fcd2a721b75217a0ab2559
manaskumar <manaskumar82499@gmail.com>
2024-04-04 15:38:40 +0530

    first

---
+++ index.js
@@ -0,0 +1,52 @@
+let red = document.querySelector("#red");
+let blue = document.querySelector("#blue");
+let black = document.querySelector("#black");
+let pink = document.querySelector("#pink");
+let white = document.querySelector("#white");
+let brown = document.querySelector("#brown");
+let gray = document.querySelector("#gray");
+let yellow = document.querySelector("#yellow");
+let orange = document.querySelector("#orange");
+let navy = document.querySelector("#navy");
+
+
+
+red.addEventListener("click", () => {
+    document.body.style.backgroundColor = "red";
+});
+
+blue.addEventListener("click", () => {
+    document.body.style.backgroundColor = "blue";
+});
+
+black.addEventListener("click", () => {
+    document.body.style.backgroundColor = "black";
+});
+
+pink.addEventListener("click", () => {
+    document.body.style.backgroundColor = "pink";
+});
+
+white.addEventListener("click", () => {
+    document.body.style.backgroundColor = "white";
+});
+
+brown.addEventListener("click", () => {
+    document.body.style.backgroundColor = "brown";
+});
+
+gray.addEventListener("click", () => {
+    document.body.style.backgroundColor = "gray";
+});
+
+yellow.addEventListener("click", () => {
+    document.body.style.backgroundColor = "yellow";
+});
+
+orange.addEventListener("click", () => {
+    document.body.style.backgroundColor = "orange";
+});
+
+navy.addEventListener("click", () => {
+    document.body.style.backgroundColor = "navy";
+});
